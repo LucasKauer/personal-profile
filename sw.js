9bb529a9bce296714ff41ca02c1bf10cc0f120bd
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-01-09 08:22:03 -0200

    fix(sw): rename urls to cache of sw

--- sw.js
+++ sw.js
@@ -2,17 +2,17 @@ var CACHE_NAME = 'personal-profile-v3';
 
 var urlsToCache = [
   '/',
-  '/index.html',  
-  '/stylesheets/main.css',
-  '/javascripts/jquery-2.1.4.min.js',
-  '/javascripts/main.js',
-  '/images/facebook.png',
-  '/images/github.png',
-  '/images/linkedin.png',
-  '/images/mail.png',
-  '/images/stackoverflow.png',
-  '/images/whatsapp.png',
-  '/fonts/Rajdhani/Rajdhani-Regular.ttf'
+  'index.html',  
+  'stylesheets/main.css',
+  'javascripts/jquery-2.1.4.min.js',
+  'javascripts/main.js',
+  'images/facebook.png',
+  'images/github.png',
+  'images/linkedin.png',
+  'images/mail.png',
+  'images/stackoverflow.png',
+  'images/whatsapp.png',
+  'fonts/Rajdhani/Rajdhani-Regular.ttf'
 ];
 
 self.addEventListener('install', function(event) {
